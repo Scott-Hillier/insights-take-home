67051753a0357db8c09a3060c74d64b94f77244a
Scott-Hillier <scotthhillier@gmail.com>
2022-10-31 14:49:46 -0700

    updated logic for creating JSON file

--- technical-assessment/src/components/Download.js
+++ technical-assessment/src/components/Download.js
@@ -4,21 +4,21 @@ import dogJSON from "../logic/dogJSON";
 
 const Download = () => {
   const dogData = useSelector((state) => state.dogs.value);
-  const data = dogJSON(dogData);
+  const data = dogJSON(dogData.columns, dogData.dogs);
 
-  const download = async () => {
+  const download = () => {
     const filename = "DogBreeds.json";
     const json = JSON.stringify(data);
     const blob = new Blob([json], { type: "application/json" });
-    const href = await URL.createObjectURL(blob);
+    const href = URL.createObjectURL(blob);
     const link = document.createElement("a");
     link.href = href;
     link.download = filename;
-
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
+
   return (
     <button
       id="download-button"
--- technical-assessment/src/logic/dogJSON.js
+++ technical-assessment/src/logic/dogJSON.js
@@ -1,22 +1,19 @@
-const dogJSON = (dogData) => {
+const dogJSON = (columns, dogs) => {
   const dogJSON = {
     dogBreeds: {
-      breed1Total: dogData.columns["column-1"].dogIds.length,
+      breed1Total: columns["column-1"].dogIds.length,
       breed1Rank: {},
-      breed2Total: dogData.columns["column-2"].dogIds.length,
+      breed2Total: columns["column-2"].dogIds.length,
       breed2Rank: {},
     },
   };
 
-  for (let i = 0; i < dogData.columns["column-1"].dogIds.length; i++) {
-    dogJSON.dogBreeds.breed1Rank[`rank${i + 1}`] =
-      dogData.dogs[dogData.columns["column-1"].dogIds[i]].name;
-  }
-
-  for (let i = 0; i < dogData.columns["column-2"].dogIds.length; i++) {
-    dogJSON.dogBreeds.breed2Rank[`rank${i + 1}`] =
-      dogData.dogs[dogData.columns["column-2"].dogIds[i]].name;
-  }
+  Object.keys(columns).forEach((key, i) => {
+    columns[key].dogIds.map((dogId, index) => {
+      return (dogJSON.dogBreeds[`breed${i + 1}Rank`][`rank${index + 1}`] =
+        dogs[dogId].name);
+    });
+  });
 
   return dogJSON;
 };
